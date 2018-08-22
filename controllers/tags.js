63be7835942b5151882f1d932513b3ec386196bb
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-08-22 10:15:32 -0700

    added tag navigation and ajax route stub

---
+++ controllers/tags.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.get('/', function(req, res){
+ db.tag.findAll().then(function(tags){
+ 	res.render('tags/index', { tags: tags});
+ }).catch(function(err){
+ 	console.log(err);
+ 	res.render('error');
+ });
+
+});
+
+router.get('/:id', function(req, res){
+	db.tag.findOne({
+		where: {id: req.params.id},
+		include: [db.article]
+	}).then(function(tag){
+		res.render('tags/show', {tag: tag});
+	}).catch(function(err){
+ 	console.log(err);
+ 	res.render('error');
+ });
+});
+
+
+router.delete('/:id', function(req,res){
+	res.send('DELETE! ! !');
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -9,12 +9,14 @@ var app = express();
 // set and use statements
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // include controllers
 app.use('/authors', require('./controllers/authors'));
 app.use('/articles', require('./controllers/articles'));
 app.use('/comments', require('./controllers/comments'));
+app.use('/tags', require('./controllers/tags'));
 
 // routes
 app.get('/', function(req, res){
@@ -24,6 +26,7 @@ app.get('/', function(req, res){
 app.get("*", function(req, res){
 	res.render("error");
 });
+
 
 //listening
 app.listen(3000, function(){
